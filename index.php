74adfb5fa4a5f680f9cc07d429f183f823418ae8
zhuikovatanya <zhuikovatanya@mail.ru>
2026-06-03 23:13:19 +0500

    лаб17

--- index.php
+++ index.php
@@ -1,61 +1,160 @@
 <?php
-$pageTitle = "Главная страница";
-$pageHeading = "Добро пожаловать!";
-$currentYear = date('Y');
+//1 задание
 
-function getCurrentTime(): string {
-    $hour = (int)date('H');
-    $minute = (int)date('i');
+$a = 15;
+$b = 0;
 
-    $hourStr = $hour;
-    if ($hour === 1 || $hour === 21) {
-        $hourStr .= " час";
-    } elseif (($hour >= 2 && $hour <= 4) || ($hour >= 22 && $hour <= 24)) {
-        $hourStr .= " часа";
-    } else {
-        $hourStr .= " часов";
-    }
+echo "<h2>Задание 1</h2>";
+echo "Переменная \$a = $a<br>";
+echo "Переменная \$b = $b<br>";
 
-    $minuteStr = $minute;
-    if ($minute % 10 === 1 && $minute !== 11) {
-        $minuteStr .= " минута";
-    } elseif (($minute % 10 >= 2 && $minute % 10 <= 4) && ($minute < 10 || $minute > 20)) {
-        $minuteStr .= " минуты";
-    } else {
-        $minuteStr .= " минут";
-    }
-
-    return "Текущее время: $hourStr $minuteStr";
+if ($a >= 0 && $b >= 0) {
+    $result = $a - $b;
+    echo "Оба числа положительные, их разность: $result";
+} elseif ($a < 0 && $b < 0) {
+    $result = $a * $b;
+    echo "Оба числа отрицательные, их произведение: $result";
+} else {
+    $result = $a + $b;
+    echo "Числа разных знаков, их сумма: $result";
 }
 
-$currentTimeStr = getCurrentTime();
+echo "<hr>";
+
+//2 задание
+
+echo "<h2>Задание 2</h2>";
+
+$a = 3;
+
+echo "Начальное значение \$a = $a<br>";
+echo "Вывод чисел от $a до 15: ";
+
+switch ($a) {
+    case 0: echo "0 ";
+    case 1: echo "1 ";
+    case 2: echo "2 ";
+    case 3: echo "3 ";
+    case 4: echo "4 ";
+    case 5: echo "5 ";
+    case 6: echo "6 ";
+    case 7: echo "7 ";
+    case 8: echo "8 ";
+    case 9: echo "9 ";
+    case 10: echo "10 ";
+    case 11: echo "11 ";
+    case 12: echo "12 ";
+    case 13: echo "13 ";
+    case 14: echo "14 ";
+    case 15: echo "15 ";
+        break;
+    default:
+        echo "Число не входит в диапазон [0..15]";
+}
+
+echo "<hr>";
+
+//3 задание
+
+echo "<h2>Задание 3</h2>";
+
+function addition($arg1, $arg2) {
+    return $arg1 + $arg2;
+}
+
+function subtraction($arg1, $arg2) {
+    return $arg1 - $arg2;
+}
+
+function multiplication($arg1, $arg2) {
+    return $arg1 * $arg2;
+}
+
+function division($arg1, $arg2) {
+    if ($arg2 == 0) {
+        return "Ошибка: деление на ноль";
+    }
+    return $arg1 / $arg2;
+}
+
+$num1 = 8;
+$num2 = 2;
+
+echo "Число 1: $num1<br>";
+echo "Число 2: $num2<br>";
+echo "Сложение: " . addition($num1, $num2) . "<br>";
+echo "Вычитание: " . subtraction($num1, $num2) . "<br>";
+echo "Умножение: " . multiplication($num1, $num2) . "<br>";
+echo "Деление: " . division($num1, $num2) . "<br>";
+
+echo "<hr>";
+
+//4 задание
+
+echo "<h2>Задание 4</h2>";
+
+function mathOperation($arg1, $arg2, $operation) {
+    switch ($operation) {
+        case 'сложение':
+            return addition($arg1, $arg2);
+        case 'вычитание':
+            return subtraction($arg1, $arg2);
+        case 'умножение':
+            return multiplication($arg1, $arg2);
+        case 'деление':
+            return division($arg1, $arg2);
+        default:
+            return "Неизвестная операция";
+    }
+}
+
+$num1 = 15;
+$num2 = 3;
+
+echo "Число 1: $num1<br>";
+echo "Число 2: $num2<br>";
+echo "Операция «сложение»: " . mathOperation($num1, $num2, 'сложение') . "<br>";
+echo "Операция «вычитание»: " . mathOperation($num1, $num2, 'вычитание') . "<br>";
+echo "Операция «умножение»: " . mathOperation($num1, $num2, 'умножение') . "<br>";
+echo "Операция «деление»: " . mathOperation($num1, $num2, 'деление') . "<br>";
+
+echo "<hr>";
+
+//5 задание
+
+echo "<h2>Задание 5</h2>";
+
+echo "Функция date(): " . date("Y") . "<br>";
+
+$date_array = getdate();
+echo "Функция getdate(): " . $date_array['year'] . "<br>";
+
+$datetime = new DateTime();
+echo "Класс DateTime: " . $datetime->format('Y') . "<br>";
+
+echo "<hr>";
+
+//6 задание
+
+echo "<h2>Задание 6</h2>";
+
+function power($val, $pow) {
+    if ($pow == 0) {
+        return 1;
+    }
+
+    if ($pow < 0) {
+        return 1 / power($val, -$pow);
+    }
+
+    return $val * power($val, $pow - 1);
+}
+
+$base = 2;
+$exponents = [-1, 0, 1, 2, 3];
+
+echo "Основание: $base<br>";
+foreach ($exponents as $exp) {
+    echo "$base в степени $exp = " . power($base, $exp) . "<br>";
+}
 ?>
-
-<!doctype html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php echo $pageTitle; ?></title>
-    <link rel="stylesheet" href="/src/assets/styles/style.css">
-</head>
-<body>
-    <header>
-        <h1><?php echo $pageHeading; ?></h1>
-    </header>
-
-    <main>
-        <div class="time-container">
-            <p><?php echo $currentTimeStr; ?></p>
-        </div>
-    </main>
-
-    <footer>
-        <p>&copy; <?php echo $currentYear; ?> Все права защищены</p>
-    </footer>
-
-    <script src="/src/index.js"></script>
-</body>
-</html>
